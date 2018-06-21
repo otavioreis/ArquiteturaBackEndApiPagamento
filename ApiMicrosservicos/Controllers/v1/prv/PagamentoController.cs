6aef130c4872a422c91eaa2fcc82c97b6bdba343
Otávio Augusto de Queiroz Reis <otavioreis@gmail.com>
2018-06-21 20:51:35 -0300

    Add project files.

---
+++ ApiMicrosservicos/Controllers/v1/prv/PagamentoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Swashbuckle.AspNetCore.Examples;
+using System.Net;
+using System.Threading.Tasks;
+using Livraria.Api.ObjectModel;
+
+namespace Livraria.Api.Controllers.v1.prv
+{
+    [Route("v1/private/[controller]")]
+    public partial class PagamentoController : Controller
+    {
+        /// <summary>
+        /// Se o valor for 5510 4481 5196 2381 o valor será true, caso contrário, false
+        /// </summary>
+        /// <param name="dadosPagamento">Dados pagamento enviado no corpo da página</param>
+        /// <response code="200">True</response>
+        /// <response code="400">False</response>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] DadosPagamento dadosPagamento)
+        {
+            if(dadosPagamento != null)
+            {
+                if (dadosPagamento.NumeroCartao == "5510 4481 5196 2381")
+                {
+                    return Ok(true);
+                }
+            }
+
+            return Ok(false);
+        }
+    }
+}
---
+++ ApiMicrosservicos/Startup.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Examples;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+
+namespace Livraria.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "API de Pagamento",
+                    Description = "Um exemplo de endpoints de uma API mock de pagamento construída com ASP.NET Core Web API",
+                    Contact = new Contact
+                    {
+                        Name = "Otávio Augusto de Queiroz Reis",
+                        Email = "otavioreis@gmail.com",
+                        Url = String.Empty
+                    },
+                    License = new License
+                    {
+                        Name = "Licença MIT",
+                        Url = "https://raw.githubusercontent.com/otavioreis/ArquiteturaBackEndApiPagamento/master/LICENSE"
+                    }
+                });
+                c.IncludeXmlComments(GetXmlCommentsPath());
+                c.OperationFilter<ExamplesOperationFilter>(); // [SwaggerRequestExample] & [SwaggerResponseExample]
+                c.OperationFilter<DescriptionOperationFilter>(); // [Description] on Response properties
+            });
+
+            services.AddMvc();
+        }
+
+        protected static string GetXmlCommentsPath()
+        {
+            return System.String.Format(@"{0}\Swagger.xml", System.AppDomain.CurrentDomain.BaseDirectory);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pagamento API V1");
+            });
+
+            app.UseMvc();
+        }
+
+
+    }
+}
